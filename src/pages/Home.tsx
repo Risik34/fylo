e696e1e9109afd768ac28ead20c5f89eae677112
Risik34 <Risimn7@gmail.com>
2024-07-01 18:55:01 +0530

    Finished features section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,13 @@
-import Navbar from '../components/Navbar';
-import illustrationIntro from '../assets/illustration-intro.png';
-
-const Title = () => {
-  return (
-    <div className="px-8 text-center">
-      <img src={illustrationIntro} />
-      <h1 className="text-2xl font-raleway font-semibold my-2">
-        All your files in one secure location, accessible anywhere.
-      </h1>
-      <p>
-        Fylo stores all your most important files in one secure location. Access
-        them wherever you need, share and collaborate with friends family, and
-        co-workers.
-      </p>
-      <button className="justify-center items-center px-20 py-3 m-3 rounded-full bg-gradient-to-r from-cyan to-blue">
-        Get started
-      </button>
-    </div>
-  );
-};
-
+import Cta from '/src/components/cta/Cta';
+import Feature from '/src/components/feature/Feature';
+import HowItWorks from '/src/components/HowItWorks';
 const Home = () => {
   return (
-    <div className="">
-      <Navbar></Navbar>
-      <Title />
-    </div>
+    <>
+      <Cta />
+      <Feature />
+      <HowItWorks />
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     // colors: {
     // },
     extend: {
       colors: {
-        bg: "#181f2a",
-        footerbg: "#0b1523",
-        testimonialsbg: "#202a3c",
+        bg: '#181f2a',
+        footerbg: '#0b1523',
+        testimonialsbg: '#202a3c',
 
-        cyan: "#65e2d9",
-        blue: "#339ecc",
+        cyan: '#65e2d9',
+        blue: '#339ecc',
       },
 
       fontFamily: {
-        raleway: "Raleway ,sans-serif",
-        opensans: "Open sans , sans-serif",
+        raleway: 'Raleway ,sans-serif',
+        opensans: 'Open sans , sans-serif',
+      },
+      screens: {
+        sm: '300px',
+        md: '768px', // Starting sm at 768px
+        lg: '1280px', // You can adjust lg as well if needed
+        // Add or adjust other breakpoints as necessary
       },
     },
   },
